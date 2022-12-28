7a9289b7f4b6fedfe975f0b9cd99f3d04f3f1f09
skeyen <stevekeyen@gmail.com>
2022-12-28 11:27:14 +0100

    added tailwind

---
+++ targetworx/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dashboard/src/**/*.{html,ts}', './login/src/**/*.{html,ts}','./todo/src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
